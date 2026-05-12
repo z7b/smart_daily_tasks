13cc2aad5e3d8d36b97fea5f90270da1ea1a250e
z7b <42646553+z7b@users.noreply.github.com>
2026-05-12 06:48:13 +0300

    fix: enable native Material localizations for date picker

--- lib/main.dart
+++ lib/main.dart
@@ -358,8 +358,9 @@ class MainApp extends StatelessWidget {
         initialBinding: InitialBinding(),
         getPages: AppPages.routes,
         translations: Messages(),
-        locale: Locale(themeService.getLocale().languageCode),
+        locale: themeService.getLocale(),
         fallbackLocale: const Locale('en'),
+        supportedLocales: ThemeService.supportedLocales.values.toList(),
         localizationsDelegates: const [
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
